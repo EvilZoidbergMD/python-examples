b93f7e5096b6db0224aa0a63b52ac49ae5aa840c
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-17 13:37:17 -0600

    Added madlib.py

---
+++ Intermediate/madlib.py
@@ -0,0 +1,30 @@
+
+def main():
+    #First get the inputs
+    noun1 = raw_input('Give me a noun >>> ')
+    verb1 = raw_input('Give me a verb >>> ')
+    adjective1 = raw_input('Give me a adjective >>> ')
+    verb2 = raw_input('Give me a verb >>> ')
+    noun2 = raw_input('Give me a noun >>> ')
+    noun3 = raw_input('Give me a noun >>> ')
+    ajective2 = raw_input('Give me a adjective >>> ')
+    noun4 = raw_input('Give me a noun >>> ')
+
+    print('\n')
+
+    print('There once was a ' + noun1 + ' that liked to ' + verb1 + ',')
+    print('but the ' + adjective1 + ' ' + noun2 + ' did not like that.')
+    print('The ' + noun1 + ' tried to ' + verb2 + ' with the ' + noun2 + ' but it was no use.')
+    print('Along came a ' + noun3 + ' who said to the ' + noun1 + ' you really should not do that.')
+    print('The ' + noun1 + ' looked ' + ajective2 + ' and realized that it needed to give its ' + noun4)
+    print('to the ' + noun2 + '.')
+    
+    print('\n')
+
+    print('The End')
+
+    print('\n')
+
+
+if __name__ == '__main__':
+    main()
